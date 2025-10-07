2e021eb12593af29e98f966ae453f1b53dc1e67b
bauer <clemens.bauer@it.htlhl.at>
2025-10-07 08:03:17 +0200

    homework

--- opengl/init-buffers.js
+++ opengl/init-buffers.js
@@ -1,104 +1,71 @@
 function initBuffers(gl) {
-    const positionBuffer = initPositionBuffer(gl);
-    const colorBuffer = initColorBuffer(gl);
-    const indexBuffer = initIndexBuffer(gl);
+  const positionBuffer = initPositionBuffer(gl);
+  const colorBuffer = initColorBuffer(gl);
+  const indexBuffer = initIndexBuffer(gl);
+  return {
+    position: positionBuffer,
+    color: colorBuffer,
+    indices: indexBuffer,
+  };
+}
 
-    return {
-      position: positionBuffer,
-      color: colorBuffer,
-      indices: indexBuffer,
-    };
-  }
+function initPositionBuffer(gl) {
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   
-  function initPositionBuffer(gl) {
-    // Create a buffer for the square's positions.
-    const positionBuffer = gl.createBuffer();
+  const positions = [
+    -1.0, -1.0, -1.0,  // 0
+     1.0, -1.0, -1.0,  // 1
+     1.0,  1.0, -1.0,  // 2
+    -1.0,  1.0, -1.0,  // 3
+    -1.0, -1.0,  1.0,  // 4
+     1.0, -1.0,  1.0,  // 5
+     1.0,  1.0,  1.0,  // 6
+    -1.0,  1.0,  1.0,  // 7
+  ];
   
-    // Select the positionBuffer as the one to apply buffer
-    // operations to from here out.
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+  return positionBuffer;
+}
+
+function initColorBuffer(gl) {
+  const colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
   
-    // Now create an array of positions for the square.
-    const positions = [
-      // Front face
-      -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
-    
-      // Back face
-      -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
-    
-      // Top face
-      -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
-    
-      // Bottom face
-      -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
-    
-      // Right face
-      1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
-    
-      // Left face
-      -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
-    ];
+  const colors = [
+    0.0, 0.0, 0.0, 1.0,  
+    1.0, 0.0, 0.0, 1.0,  
+    1.0, 1.0, 0.0, 1.0,  
+    0.0, 1.0, 0.0, 1.0,  
+    1.0, 0.0, 1.0, 1.0,  
+    0.0, 1.0, 1.0, 1.0,  
+    0.0, 0.0, 1.0, 1.0,  
+    1.0, 1.0, 1.0, 1.0,  
+  ];
   
-    // Now pass the list of positions into WebGL to build the
-    // shape. We do this by creating a Float32Array from the
-    // JavaScript array, then use it to fill the current buffer.
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  return colorBuffer;
+}
+
+function initIndexBuffer(gl) {
+  const indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+ 
+  const indices = [
+    4, 5, 6,  4, 6, 7,  
+    1, 0, 3,  1, 3, 2,  
+    7, 6, 2,  7, 2, 3,  
+    4, 0, 1,  4, 1, 5,  
+    5, 1, 2,  5, 2, 6,  
+    0, 4, 7,  0, 7, 3,  
+  ];
   
-    return positionBuffer;
-  }
-  function initColorBuffer(gl) {
-    const faceColors = [
-      [1.0, 1.0, 1.0, 1.0], // Front face: white
-      [1.0, 0.0, 0.0, 1.0], // Back face: red
-      [0.0, 1.0, 0.0, 1.0], // Top face: green
-      [0.0, 0.0, 1.0, 1.0], // Bottom face: blue
-      [1.0, 1.0, 0.0, 1.0], // Right face: yellow
-      [1.0, 0.0, 1.0, 1.0], // Left face: purple
-    ];
-    
-    // Convert the array of colors into a table for all the vertices.
-    
-    let colors = [];
-    
-    for (const c of faceColors) {
-      // Repeat each color four times for the four vertices of the face
-      colors = colors.concat(c, c, c, c);
-    }
-    
-    const colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-  
-    return colorBuffer;
-  }
-  function initIndexBuffer(gl) {
-    const indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  
-    // This array defines each face as two triangles, using the
-    // indices into the vertex array to specify each triangle's
-    // position.
-  
-    // prettier-ignore
-    const indices = [
-       0,  1,  2,      0,  2,  3,    // front
-       4,  5,  6,      4,  6,  7,    // back
-       8,  9,  10,     8,  10, 11,   // top
-       12, 13, 14,     12, 14, 15,   // bottom
-       16, 17, 18,     16, 18, 19,   // right
-       20, 21, 22,     20, 22, 23,   // left
-    ];
-  
-    // Now send the element array to GL
-  
-    gl.bufferData(
-      gl.ELEMENT_ARRAY_BUFFER,
-      new Uint16Array(indices),
-      gl.STATIC_DRAW,
-    );
-  
-    return indexBuffer;
-  }
-  
-  
-  export { initBuffers };
+  gl.bufferData(
+    gl.ELEMENT_ARRAY_BUFFER,
+    new Uint16Array(indices),
+    gl.STATIC_DRAW
+  );
+  return indexBuffer;
+}
+
+export { initBuffers };
